a6b855c9b4725677494b47c16ee4da3b4fd1fd34
kwonhyucknae <nhk321@naver.com>
2017-02-15 01:26:51 +0900

    초안

---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public enum playerState
+    {
+        idle=0,
+        walk
+    }
+
+    public Animator _anim;
+    public playerState _state = playerState.walk;
+    public float _speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(_speed * Time.deltaTime, 0.0f, 0.0f);
+
+        if(_anim!=null)
+        {
+            _anim.SetInteger("PlayerState", (int)_state);
+        }
+	}
+}
